3212f001f88cb668c9e63b003f78ffcd13fd52ad
RK <108023178+RAHUL-KAR-Kalyani@users.noreply.github.com>
2024-04-19 20:19:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 function shortcut(s1, s2) {
   // your code here
+	if (str1.length === 0 || str2.length === 0) {
+        return '';
+    } else {
+        return str1[0].toUpperCase() + str2[0].toLowerCase();
+    }
 }
-
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
 const s2 = prompt("Enter s2:");
